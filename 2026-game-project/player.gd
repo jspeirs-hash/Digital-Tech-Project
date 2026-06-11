77427c1e7e7f05b94d56f660f67664067c59b33e
Jaymie <jspeirs@students.mags.school.nz>
2026-06-12 09:55:04 +1200

    Assests for testing
    adding some free assest testing for isometric map

---
+++ 2026-game-project/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed: float = 300.0
+var sprint: float = 500.0
+
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2(0.0,0.0)
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+
+	velocity = speed * direction.normalized()
+	if Input.is_action_pressed("Sprint"):
+		speed = sprint
+	move_and_slide()
+
+func sprints(delta: float) -> void:
+	print()
