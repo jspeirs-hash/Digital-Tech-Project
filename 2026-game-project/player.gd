d174179d828f9e98f84856c0a690d9de058dd41e
Jaymie <jspeirs@students.mags.school.nz>
2026-06-16 09:30:54 +1200

    Sprinting | Mechanic
    coding the sprinting for the mechanic of the game

--- 2026-game-project/player.gd
+++ 2026-game-project/player.gd
@@ -1,18 +1,36 @@
 extends CharacterBody2D
 
 var speed: float = 300.0
-var sprint: float = 500.0
+var walk_speed: float = 300.0
+var sprint_speed: float = 500.0
+var dash_distance: float = 1000.0
 var can_sprint: = false
+var can_dash = false
 
 func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2(0.0,0.0)
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
 
-	velocity = speed * direction.normalized()
-	if Input.is_action_pressed("Sprint"):
-		speed = sprint
+	
+	#sprinting mechanic
+	if Input.is_action_just_pressed("Sprint"):
+		can_sprint = true
+		if can_sprint:
+			walk_speed = sprint_speed
+	elif Input.is_action_just_released("Sprint"):
+		can_sprint = false
+		walk_speed = speed
+	
+	#dashing mechanic
+	if Input.is_action_just_pressed("Dash"):
+		can_dash = true
+		if can_dash:
+			velocity = dash_distance * direction
+			print("dash")
+	else:
+		can_dash = false
+		
+	velocity = walk_speed * direction.normalized()
+	
 	move_and_slide()
-
-func sprints(delta: float) -> void:
-	print()
