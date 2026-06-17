d0d8939d62a1171519b870b59e05c2692a255c72
Jaymie <jspeirs@students.mags.school.nz>
2026-06-17 17:28:47 +1200

    Dashing / fix a little on Sprints
    Adding @export to adjust easier and changing a new Vector look but still work like it use to | Movement mechanic fisnish

--- 2026-game-project/player.gd
+++ 2026-game-project/player.gd
@@ -1,36 +1,59 @@
 extends CharacterBody2D
 
-var speed: float = 300.0
-var walk_speed: float = 300.0
-var sprint_speed: float = 500.0
-var dash_distance: float = 1000.0
-var can_sprint: = false
-var can_dash = false
+@export var walk_speed: float = 260.0
+@export var sprint_speed: float = 320.0
+@export var dash_speed: float = 1000.0
+@export var dash_duration: float = 3.0
+@export var dash_cooldown: float = 3.0
 
-func _process(delta: float) -> void:
-	var direction: Vector2 = Vector2(0.0,0.0)
+var current_speed = walk_speed
+var dash_direction: Vector2 = Vector2.ZERO
+var can_dash = true
+var is_dashing = false
+var can_sprint: = false
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
-
 	
-	#sprinting mechanic
-	if Input.is_action_just_pressed("Sprint"):
-		can_sprint = true
-		if can_sprint:
-			walk_speed = sprint_speed
-	elif Input.is_action_just_released("Sprint"):
-		can_sprint = false
-		walk_speed = speed
-	
-	#dashing mechanic
-	if Input.is_action_just_pressed("Dash"):
-		can_dash = true
-		if can_dash:
-			velocity = dash_distance * direction
-			print("dash")
-	else:
-		can_dash = false
-		
 	velocity = walk_speed * direction.normalized()
 	
+	#dashing mechanic
+	if Input.is_action_just_pressed("Dash") and can_dash and direction != Vector2.ZERO:
+		
+		is_dashing = true
+		can_dash = false
+		dash_direction = direction.normalized()
+		
+		var Cooldown_timer = get_node("Cooldown_dash")
+		Cooldown_timer.wait_time = dash_cooldown
+		Cooldown_timer.start()
+		
+		var Duration_timer = get_node("Duration_dash")
+		Duration_timer.wait_time = dash_duration
+		Duration_timer.start()
+		
+	if is_dashing:
+		velocity = dash_direction * dash_speed
+	else:
+		#sprinting mechanic
+		if Input.is_action_just_pressed("Sprint"):
+			can_sprint = true
+			if can_sprint:
+				walk_speed = sprint_speed
+		elif Input.is_action_just_released("Sprint"):
+			can_sprint = false
+			walk_speed = current_speed
 	move_and_slide()
+
+func _Duration_Dash_Timeout() -> void:
+	is_dashing = false
+	print("Duration")
+
+func _Cooldown_Dash_Timeout() -> void:
+	can_dash = true
+	print("Cooldown")
